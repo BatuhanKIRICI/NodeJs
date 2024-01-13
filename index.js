1ee6a0ad61aeed9e19f2edc2ab382f6f44c25fde
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-13 04:31:06 +0300

    "Asynchronous"

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 
-const textIn = fs.readFileSync("./1-node-farm/starter/txt/input.txt", "utf-8");
+// Synchronous
+
+/* const textIn = fs.readFileSync("./1-node-farm/starter/txt/input.txt", "utf-8");
 
 const textOut = `This is what we know about avacado: ${textIn}.\nCreated on ${Date.now()}`;
 
@@ -8,4 +10,33 @@ console.log(textOut);
 
 fs.writeFileSync("./1-node-farm/starter/txt/output.txt", textOut);
 
-console.log("File written!");
+console.log("File written!"); */
+
+// Asynchronous
+
+fs.readFile("./1-node-farm/starter/txt/start.txt", "utf-8", (err, data1) => {
+  if (err) console.log("Error!");
+  fs.readFile(
+    `./1-node-farm/starter/txt/${data1}.txt`,
+    "utf-8",
+    (err, data2) => {
+      //   console.log(data2);
+      fs.readFile(
+        "./1-node-farm/starter/txt/append.txt",
+        "utf-8",
+        (err, data3) => {
+          console.log(data3);
+          fs.writeFile(
+            "./1-node-farm/starter/txt/final.txt",
+            `${data2}\n${data3}`,
+            "utf-8",
+            (err) => {
+              console.log("Your file has been written!");
+            }
+          );
+        }
+      );
+    }
+  );
+});
+console.log("Will read the file!");
