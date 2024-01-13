76309b0a7c82d6eb3685747229ad1e4b975e73da
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-13 03:05:10 +0300

    "Reading and writing files"

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const hello = "Hello World!";
-console.log(hello);
+const fs = require("fs");
+
+const textIn = fs.readFileSync("./1-node-farm/starter/txt/input.txt", "utf-8");
+
+const textOut = `This is what we know about avacado: ${textIn}.\nCreated on ${Date.now()}`;
+
+console.log(textOut);
+
+fs.writeFileSync("./1-node-farm/starter/txt/output.txt", textOut);
+
+console.log("File written!");
