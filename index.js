c63a1506a77b52ba42dca7b02aecbbf6ec7e1ed8
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-13 17:11:03 +0300

    "Routing"

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 // Synchronous
 
@@ -45,8 +46,19 @@ console.log("Will read the file!"); */
 //SERVER
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  res.end("Hello from the server!");
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/about") {
+    res.end("This is ABOUT section");
+  } else if (pathName === "/product") {
+    res.end("This is PRODUCT section");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "Hello World!",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
