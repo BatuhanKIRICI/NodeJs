f951138e1f96ddcf59fbf669c5e817ab81fe7cb6
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-16 23:56:58 +0300

    "process error"

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const fs = require("fs");
 
-fs.readFile("./files/lorem.txt", (err, data) => {
+fs.readFile("./files/lorem1.txt", "utf-8", (err, data) => {
   if (err) throw err;
-  console.log(data.toString());
+  console.log(data);
+});
+
+process.on("uncaughtException", (err) => {
+  console.log(`There was an uncaught error: ${err}`);
+  process.exit(1);
 });
