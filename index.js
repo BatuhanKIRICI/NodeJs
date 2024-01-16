b3d95b605687b6b9943ca3f9fde66e8738c3fefb
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-17 00:14:47 +0300

    "Write and append"

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const fs = require("fs");
+const path = require("path");
 
-fs.readFile("./files/starter.txt", "utf-8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+fs.readFile(
+  path.join(__dirname, "files", "starter.txt"),
+  "utf-8",
+  (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  }
+);
 
 console.log("Hello...");
+
+fs.writeFile(
+  path.join(__dirname, "files", "reply.txt"),
+  "Nice to meet you pal!",
+  (err) => {
+    if (err) throw err;
+    console.log("Writing completed!");
+  }
+);
+
+fs.appendFile(
+  path.join(__dirname, "files", "test.txt"),
+  "Testing text!",
+  (err) => {
+    if (err) throw err;
+    console.log("Append completed!");
+  }
+);
 
 process.on("uncaughtException", (err) => {
   console.log(`There was an uncaught error: ${err}`);
