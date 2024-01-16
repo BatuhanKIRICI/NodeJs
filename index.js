4cb33541431e36a5cc68a349ddf4f1e2663ef45a
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-16 22:26:45 +0300

    "readfile"

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-// console.log("Hello World!");
+const fs = require("fs");
 
-// console.log(global);
-
-const os = require("os");
-const path = require("path");
-const math = require("./math");
-const { add, subtract, multiply, divide } = require("./math");
-
-// console.log(os.version());
-// console.log(os.type());
-// console.log(os.homedir());
-
-// console.log(__dirname);
-// console.log(__filename);
-
-// console.log(path.dirname(__filename));
-// console.log(path.parse(__filename));
-console.log(math.multiply(2, 5));
-console.log(math.subtract(15, 8));
-console.log(divide(88, 4));
+fs.readFile("./files/lorem1.txt", "utf-8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
