d0615b3dda6067bbbfbf73717eac4fbedb4b9d4d
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-17 00:05:21 +0300

    "Async"

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 
-fs.readFile("./files/lorem1.txt", "utf-8", (err, data) => {
+fs.readFile("./files/starter.txt", "utf-8", (err, data) => {
   if (err) throw err;
   console.log(data);
 });
+
+console.log("Hello...");
 
 process.on("uncaughtException", (err) => {
   console.log(`There was an uncaught error: ${err}`);
