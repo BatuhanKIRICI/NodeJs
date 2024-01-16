3b2d4efb68def224a41b641eff00d7abf7c7c443
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-16 23:52:48 +0300

    "toString()"

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-fs.readFile("./files/lorem1.txt", "utf-8", (err, data) => {
+fs.readFile("./files/lorem.txt", (err, data) => {
   if (err) throw err;
-  console.log(data);
+  console.log(data.toString());
 });
