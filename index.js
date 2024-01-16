0749f9053f54e164d859a2d189a235fe7bcee000
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-17 00:23:53 +0300

    "Rename"

--- index.js
+++ index.js
@@ -18,17 +18,35 @@ fs.writeFile(
   (err) => {
     if (err) throw err;
     console.log("Writing completed!");
+
+    fs.appendFile(
+      path.join(__dirname, "files", "reply.txt"),
+      "\nTesting text!",
+      (err) => {
+        if (err) throw err;
+        console.log("Append completed!");
+
+        fs.rename(
+          path.join(__dirname, "files", "reply.txt"),
+          path.join(__dirname, "files", "newReply.txt"),
+          (err) => {
+            if (err) throw err;
+            console.log("Rename completed!");
+          }
+        );
+      }
+    );
   }
 );
 
-fs.appendFile(
+/* fs.appendFile(
   path.join(__dirname, "files", "test.txt"),
   "Testing text!",
   (err) => {
     if (err) throw err;
     console.log("Append completed!");
   }
-);
+); */
 
 process.on("uncaughtException", (err) => {
   console.log(`There was an uncaught error: ${err}`);
