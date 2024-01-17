e4e73f3b1376f8d5f03b7dce07e7e4384c6cb56d
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-17 09:22:57 +0300

    "rmdir"

--- dir.js
+++ dir.js
@@ -1,8 +1,15 @@
 const fs = require("fs");
 
-if (!fs.existsSync("./new1")) {
-  fs.mkdir("./new1", (err) => {
+if (!fs.existsSync("./new")) {
+  fs.mkdir("./new", (err) => {
     if (err) throw err;
     console.log("Directory created!");
+  });
+}
+
+if (fs.existsSync("./new")) {
+  fs.rmdir("./new", (err) => {
+    if (err) throw err;
+    console.log("Directory deleted!");
   });
 }
