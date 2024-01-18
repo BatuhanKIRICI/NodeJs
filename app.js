3b59d2eee02fbfcf86ce877197ea86c2da72f6a2
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-18 23:08:50 +0300

    "Request & Response"

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((request, response) => {
-  console.log(request.url, request.method);
-  console.log(response.statusCode);
+  //   console.log(request.url, request.method);
+  //   console.log(response.statusCode);
+
+  response.setHeader("Content-Type", "text/html");
+  response.statusCode = 200;
+  response.statusMessage = "OK";
+
+  response.write("<h1>Main Page</h1>");
+  response.write("<p>Products</p>");
+
   response.end();
 });
 
