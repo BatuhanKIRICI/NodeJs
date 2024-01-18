3130f90956aebc7f11d7d844841f10fe39b094f7
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-19 00:13:12 +0300

    "File system"

--- app.js
+++ app.js
@@ -1,45 +1,28 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((request, response) => {
   if (request.url === "/") {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write(`
-        <html>
-            <head>
-                <title>Main Page</title>
-            </head>
-            <body>
-                <h1>Main Page</h1>
-            </body>
-        </html>`);
+    fs.readFile("index.html", (error, html) => {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(html);
 
-    response.end();
+      response.end();
+    });
   } else if (request.url === "/products") {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write(`
-        <html>
-            <head>
-                <title>Products</title>
-            </head>
-            <body>
-                <h2>Products</h2>
-            </body>
-        </html>`);
+    fs.readFile("products.html", (error, html) => {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(html);
 
-    response.end();
+      response.end();
+    });
   } else {
-    response.writeHead(404, { "Content-Type": "text/html" });
-    response.write(`
-        <html>
-            <head>
-                <title>404</title>
-            </head>
-            <body>
-                <h1>Page not found!</h1>
-            </body>
-        </html>`);
+    fs.readFile("404.html", (error, html) => {
+      response.writeHead(404, { "Content-Type": "text/html" });
+      response.write(html);
 
-    response.end();
+      response.end();
+    });
   }
 });
 
